41a5faaec9ef2f3176198802afbfc0bddcc7196b
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2020-09-04 21:58:17 -0400

    Add lab01 folder
    lab01 student folder

---
+++ labs/lab01/student/tests/q4_1.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q4_1',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> above_eight.num_rows == 20\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> # Make sure you're columns are in the correct order!;\n>>> above_eight.first('Title') == 'Gone Girl'\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
