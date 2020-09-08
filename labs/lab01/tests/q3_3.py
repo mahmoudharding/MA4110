752b6add9e36c3b5af97c24759996742ae5f9d08
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2020-09-08 18:42:38 -0400

    redo lab01

--- labs/lab01/tests/q3_3.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q3_3',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> num_farmers_markets_rows == farmers_markets.num_rows\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
