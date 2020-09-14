250545d920659a622bf3a2511aa1c07e90e17451
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2020-09-14 19:34:31 -0400

    lab03

---
+++ labs/lab03/tests/q1_4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_4',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': ">>> # Did you sort the movies table;\n>>> movies.first('Tickets Sold') == 1320106761.5658364\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
