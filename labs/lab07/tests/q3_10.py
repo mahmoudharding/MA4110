8d4d888a151f08aa68e80e4977f752459346e9bc
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2020-10-28 12:51:20 -0400

    Replace Lab07

--- labs/lab07/tests/q3_10.py
+++ labs/lab07/tests/q3_10.py
@@ -1,10 +1,6 @@
 test = {   'name': 'q3_10',
     'points': 1,
-    'suites': [   {   'cases': [   {   'code': '>>> np.random.seed(123);\n'
-                                               '>>> np.isclose(round(simulate_and_test_statistic(change_in_death_rates, "Death Penalty", "Murder Rate"), 3) - (-1.398), 0)\n'
-                                               'True',
-                                       'hidden': False,
-                                       'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> -5 < round(simulate_and_test_statistic(change_in_death_rates, "Death Penalty", "Murder Rate"), 3) < 5\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
