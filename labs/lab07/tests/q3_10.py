72a61ccf4db61b8a03f1032dcf66d030e9e21c4e
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2020-10-29 07:11:05 -0400

    Replace lab07

--- labs/lab07/tests/q3_10.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q3_10',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> -5 < round(simulate_and_test_statistic(change_in_death_rates, "Death Penalty", "Murder Rate"), 3) < 5\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
