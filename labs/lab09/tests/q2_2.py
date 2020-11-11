30bb05c4a1c68f59459c4b3e0ecb2d34da91a119
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2020-11-11 15:06:21 -0500

    Lab 09

--- labs/lab09/tests/q2_2.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q2_2',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> -1 < (18 + intercept*(-4)) / 201 < 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
